96b1d60778f8535ebf9e8d22b099d391da28ddcc
Venterx <akhmedov.azamat2007@gmail.com>
2024-09-28 17:52:12 +0300

    calculator 4.0, added matrix 4 by 4

--- js/main.js
+++ js/main.js
@@ -1,4 +1,33 @@
-function func() {
+window.addEventListener("load", () => {
+    document.getElementsByClassName("blocky")[0].hidden = true;
+    document.getElementsByClassName("blocky")[1].hidden = true;
+    document.getElementsByClassName("result-q")[0].hidden = true;
+    document.getElementById("counter4by4").hidden = true;
+});
+
+function removeMatrix3by3() {
+    document.getElementsByClassName('blocky')[0].hidden = true;
+    document.getElementsByClassName('blocky')[1].hidden = true;
+    document.getElementsByClassName('block')[0].hidden = false;
+    document.getElementsByClassName('block')[1].hidden = false;
+    document.getElementsByClassName("result-p")[0].hidden = false;
+    document.getElementsByClassName("result-q")[0].hidden = true;
+    document.getElementById("counter4by4").hidden = true;
+    document.getElementById("counter").hidden = false;
+}
+
+function removeMatrix4by4() {
+    document.getElementsByClassName('block')[0].hidden = true;
+    document.getElementsByClassName('block')[1].hidden = true;
+    document.getElementsByClassName('blocky')[0].hidden = false;
+    document.getElementsByClassName('blocky')[1].hidden = false;
+    document.getElementsByClassName("result-p")[0].hidden = true;
+    document.getElementsByClassName("result-q")[0].hidden = false;
+    document.getElementById("counter4by4").hidden = false;
+    document.getElementById("counter").hidden = true;
+}
+
+function counter() {
     const numbers = [
         ["num1", "num1_"],["num2", "num2_"],
         ["num3", "num3_"],["num4", "num4_"],
@@ -23,6 +52,18 @@ function func() {
         result8 = (act == '+') ? num8 + num8_ : num8 - num8_;
         result9 = (act == '+') ? num9 + num9_ : num9 - num9_;
     }
+
+    if (act == '*') {
+        result1 = (num1 * num1_) + (num2 * num4_) + (num3 * num7_);
+        result2 = (num1 * num2_) + (num2 * num5_) + (num3 * num8_);
+        result3 = (num1 * num3_) + (num2 * num6_) + (num3 * num9_);
+        result4 = (num4 * num1_) + (num5 * num4_) + (num6 * num7_);
+        result5 = (num4 * num2_) + (num5 * num5_) + (num6 * num8_);
+        result6 = (num4 * num3_) + (num5 * num6_) + (num6 * num9_);
+        result7 = (num7 * num1_) + (num8 * num4_) + (num9 * num7_);
+        result8 = (num7 * num2_) + (num8 * num5_) + (num9 * num8_);
+        result9 = (num7 * num3_) + (num8 * num6_) + (num9 * num9_);
+    }
     
     const results = [result1, result2, result3, result4, result5, result6, result7, result8, result9];
     for (let i = 0; i < results.length; i++) {
@@ -30,7 +71,74 @@ function func() {
     }
 }
 
-function func2 () {
+function counter4by4() {
+    const numbers = [
+        ["Num1", "Num1_"],["Num2", "Num2_"],
+        ["Num3", "Num3_"],["Num4", "Num4_"],
+        ["Num5", "Num5_"],["Num6", "Num6_"],
+        ["Num7", "Num7_"],["Num8", "Num8_"],
+        ["Num9", "Num9_"],["Num10", "Num10_"],
+        ["Num11", "Num11_"],["Num12", "Num12_"],
+        ["Num13", "Num13_"],["Num14", "Num14_"],
+        ["Num15", "Num15_"],["Num16", "Num16_"],
+    ];
+      
+    numbers.forEach(([Num, Num_]) => {
+        window[Num] = Number(document.getElementById(Num).value);
+        window[Num_] = Number(document.getElementById(Num_).value);
+        console.log(Num1);
+        
+    });
+      
+    if (act == '+' || act == '-') {
+        Result1 = (act == '+') ? Num1 + Num1_ : Num1 - Num1_;
+        Result2 = (act == '+') ? Num2 + Num2_ : Num2 - Num2_;
+        Result3 = (act == '+') ? Num3 + Num3_ : Num3 - Num3_;
+        Result4 = (act == '+') ? Num4 + Num4_ : Num4 - Num4_;
+        Result5 = (act == '+') ? Num5 + Num5_ : Num5 - Num5_;
+        Result6 = (act == '+') ? Num6 + Num6_ : Num6 - Num6_;
+        Result7 = (act == '+') ? Num7 + Num7_ : Num7 - Num7_;
+        Result8 = (act == '+') ? Num8 + Num8_ : Num8 - Num8_;
+        Result9 = (act == '+') ? Num9 + Num9_ : Num9 - Num9_;
+        Result10 = (act == '+') ? Num10 + Num10_ : Num10 - Num10_;
+        Result11 = (act == '+') ? Num11 + Num11_ : Num11 - Num11_;
+        Result12 = (act == '+') ? Num12 + Num12_ : Num12 - Num12_;
+        Result13 = (act == '+') ? Num13 + Num13_ : Num13 - Num13_;
+        Result14 = (act == '+') ? Num14 + Num14_ : Num14 - Num14_;
+        Result15 = (act == '+') ? Num15 + Num15_ : Num15 - Num15_;
+        Result16 = (act == '+') ? Num16 + Num16_ : Num16 - Num16_;
+
+    }
+
+    if (act == '*') {
+        Result1 = (Num1 * Num1_) + (Num2 * Num5_) + (Num3 * Num9_) + (Num4 * Num13_);
+        Result2 = (Num1 * Num2_) + (Num2 * Num6_) + (Num3 * Num10_) + (Num4 * Num14_);
+        Result3 = (Num1 * Num3_) + (Num2 * Num7_) + (Num3 * Num11_) + (Num4 * Num15_);
+        Result4 = (Num1 * Num4_) + (Num2 * Num8_) + (Num3 * Num12_) + (Num4 * Num16_);
+
+        Result5 = (Num5 * Num1_) + (Num6 * Num5_) + (Num7 * Num9_) + (Num8 * Num13_);
+        Result6 = (Num5 * Num2_) + (Num6 * Num6_) + (Num7 * Num10_) + (Num8 * Num14_);
+        Result7 = (Num5 * Num3_) + (Num6 * Num7_) + (Num7 * Num11_) + (Num8 * Num15_);
+        Result8 = (Num5 * Num4_) + (Num6 * Num8_) + (Num7 * Num12_) + (Num8 * Num16_);
+
+        Result9 = (Num9 * Num1_) + (Num10 * Num5_) + (Num11 * Num9_) + (Num12 * Num13_);
+        Result10 = (Num9 * Num2_) + (Num10 * Num6_) + (Num11 * Num10_) + (Num12 * Num14_);
+        Result11 = (Num9 * Num3_) + (Num10 * Num7_) + (Num11 * Num11_) + (Num12 * Num15_);
+        Result12 = (Num9 * Num4_) + (Num10 * Num8_) + (Num11 * Num12_) + (Num12 * Num16_);
+
+        Result13 = (Num13 * Num1_) + (Num14 * Num5_) + (Num15 * Num9_) + (Num16 * Num13_);
+        Result14 = (Num13 * Num2_) + (Num14 * Num6_) + (Num15 * Num10_) + (Num16 * Num14_);
+        Result15 = (Num13 * Num3_) + (Num14 * Num7_) + (Num15 * Num11_) + (Num16 * Num15_);
+        Result16 = (Num13 * Num4_) + (Num14 * Num8_) + (Num15 * Num12_) + (Num16 * Num16_);
+    }
+    
+    const Results = [Result1, Result2, Result3, Result4, Result5, Result6, Result7, Result8, Result9, Result10, Result11, Result12, Result13, Result14 ,Result15, Result16];
+    for (let i = 0; i < Results.length; i++) {
+        document.getElementById(`Result${i + 1}`).innerHTML = Results[i];
+    }
+}
+
+function countDeterminant () {
     const numbers = [
         ["numb1"],["numb2"],["numb3"],
         ["numb4"],["numb5"],["numb6"],
